016445e0c021c5f07201484b65711401c043d628
Robins04 <122873394+Robins04@users.noreply.github.com>
2023-05-13 09:31:12 +0200

    Update Display_driver.py

--- BSB/Source_code/Display_driver.py
+++ BSB/Source_code/Display_driver.py
@@ -1,26 +1,24 @@
 #Display driver 
 
 #Made from library using 
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
+# Substantially derived from code by (c) 2015 Richard Hull The MIT License (MIT)
+# https://github.com/rm-hull/ssd1306:
+#       "Permission is hereby granted, free of charge, to any person obtaining a copy
+#       "of this software and associated documentation files (the "Software"), to deal
+#       "in the Software without restriction, including without limitation the rights
+#       "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#       "copies of the Software, and to permit persons to whom the Software is
+#       "furnished to do so, subject to the following conditions:
+#       "The above copyright notice and this permission notice shall be included in
+#       "all copies or substantial portions of the Software."
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# B Lavery 2015:
+# This derivative "library" module is not installed to the python system as Hull's version was:
+# it simply resides alongside your own python script.
+# In this version, the I2C bus object needs to be handed in as a parameter from your user code.
+# This makes this one same library file work with either Raspberry Pi or Virtual GPIO system.
+# Hull's (clever) auto-displaying "canvas" is replaced by a persistent draw object
+# which can be incrementally changed. This canvas needs coded "display()" calls to push to the hardware.
 
 
 from lib_oled96 import ssd1306 #library for ssd1306 
